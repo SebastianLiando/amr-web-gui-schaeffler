501b4f90f1b760a37753670b965e67860a18850b
Sebastian <sebastianliando@gmail.com>
2021-05-05 17:03:44 +0800

    Move dummy data to a separate file

--- amr-web-app/src/App.js
+++ amr-web-app/src/App.js
@@ -5,6 +5,12 @@ import GeneralHealthState from './components/health-state/general-health-state/g
 import MotorStates from './components/health-state/motors-states/motor-states'
 import SensorsStates from './components/health-state/sensors-states/sensors-states'
 import Odometry from './components/nav/odometry/odometry'
+import {
+  generalHealthData,
+  motorData,
+  odometryData,
+  sensorData,
+} from './dummy/data'
 
 const useStyles = makeStyles({
   app: {
@@ -17,81 +23,15 @@ const app = () => {
 
   return (
     <Box className={classes.app}>
-      <GeneralHealthState
-        width="50%"
-        data={{
-          battery: 95.293847,
-          temperature: 37.49873,
-          distance: 54.12385,
-        }}
-      />
+      <GeneralHealthState width="50%" data={generalHealthData} />
 
-      <SensorsStates
-        width="50%"
-        data={[
-          {
-            id: 'a',
-            name: 'Sensor A',
-            status: 'ok',
-            details: 'This sensor is working properly.',
-          },
-          {
-            id: 'b',
-            name: 'Sensor B',
-            status: 'warning',
-            details:
-              'This sensor is working, but there is an indication of overheating.',
-          },
-          {
-            id: 'c',
-            name: 'Sensor C',
-            status: 'error',
-            details:
-              'This sensor is not working at all! Try restarting the sensor.',
-          },
-        ]}
-      />
-      <MotorStates
-        width="50%"
-        data={[
-          {
-            id: 'a',
-            name: 'Motor A',
-            status: 'ok',
-            details: 'This motor is working properly.',
-          },
-          {
-            id: 'b',
-            name: 'Motor B',
-            status: 'warning',
-            details:
-              'This motor is working, but there is an indication of overheating.',
-          },
-          {
-            id: 'c',
-            name: 'Motor C',
-            status: 'error',
-            details:
-              'This motor is not working at all! Try restarting the motor.',
-          },
-        ]}
-      />
+      <SensorsStates width="50%" data={sensorData} />
 
-      <div style={{ backgroundColor: 'black', width: '50%', height: '300px' }}>
-        <Odometry
-          width="30%"
-          data={{
-            yaw: 65.432123,
-            vel_yaw: 4.4234,
-            x: 32.353234,
-            vel_x: 3.124321,
-            acc_x: 3.5123,
-            y: 54.33495,
-            vel_y: 4.3432,
-            acc_y: 1.23123,
-          }}
-        />
-      </div>
+      <MotorStates width="50%" data={motorData} />
+
+      <Box style={{ backgroundColor: 'black', width: '50%', height: '300px' }}>
+        <Odometry width="30%" data={odometryData} />
+      </Box>
     </Box>
   )
 }
