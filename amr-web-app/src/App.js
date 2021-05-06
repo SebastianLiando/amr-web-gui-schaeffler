1e5483ae3405e3b268cf4d2e9efa00f6a757d105
Sebastian <sebastianliando@gmail.com>
2021-05-06 18:06:20 +0800

    Add connection to websocket

--- amr-web-app/src/App.js
+++ amr-web-app/src/App.js
@@ -1,10 +1,12 @@
 import { Box, makeStyles } from '@material-ui/core'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import GeneralHealthState from './components/health-state/general-health-state/general-health-state'
 import MotorStates from './components/health-state/motors-states/motor-states'
 import SensorsStates from './components/health-state/sensors-states/sensors-states'
 import Odometry from './components/nav/odometry/odometry'
+import StatusChip from './components/status-chip/status-chip'
+
 import {
   generalHealthData,
   motorData,
@@ -21,8 +23,52 @@ const useStyles = makeStyles({
 const app = () => {
   const classes = useStyles()
 
+  const [connected, setConnected] = useState(false)
+
+  const connectToWebSocket = () => {
+    const timeout = 5000
+    let connectTimeoutId
+
+    const ws = new WebSocket('ws://localhost:8765')
+
+    ws.onopen = () => {
+      setConnected(true)
+      console.log('Connected to websocket')
+      clearTimeout(connectTimeoutId)
+    }
+
+    ws.onmessage = () => {
+      console.log('Received message')
+    }
+
+    ws.onclose = () => {
+      setConnected(false)
+      console.log('Disconnected from websocket')
+    }
+
+    ws.onerror = err => {
+      console.log('Socket error: ' + err.message)
+      ws.close()
+
+      connectTimeoutId = setTimeout(() => {
+        console.log('Reconnecting to websocket')
+        connectToWebSocket()
+      }, timeout);
+    }
+  }
+
+  // componentDidMount - connect to websocket
+  useEffect(() => {
+    connectToWebSocket()
+  }, [])
+
   return (
     <Box className={classes.app}>
+      <StatusChip
+        title="WS"
+        type={connected === true ? 'ok' : 'error'}
+      />
+
       <GeneralHealthState width="50%" data={generalHealthData} />
 
       <SensorsStates width="50%" data={sensorData} />
