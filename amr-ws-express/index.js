314c1df8c3fce228ac9079dd5cec8d51553dc3fc
Sebastian <sebastianliando@gmail.com>
2021-06-01 11:44:48 +0800

    Add socket io

--- amr-ws-express/index.js
+++ amr-ws-express/index.js
@@ -6,10 +6,25 @@ const app = express();
 const http = require("http");
 const server = http.createServer();
 
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 app.get("/", (req, res) => {
   res.send("Up and running :)");
 });
 
-app.listen(PORT, () => {
+io.on("connection", (clientSocket) => {
+  console.log(`User ${clientSocket.id} has been connected`);
+
+  clientSocket.on("disconnect", () =>
+    console.log(`User ${clientSocket.id} has been disconnected`)
+  );
+});
+
+setInterval(() => {
+  io.emit("test", Math.random() * 1000);
+}, 2000);
+
+server.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
 });
