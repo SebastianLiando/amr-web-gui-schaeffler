5e0815d754ef265f6e1f6642eecc72558ac41f9b
Sebastian <sebastianliando@gmail.com>
2021-06-07 13:26:42 +0800

    Change dummy data to web socket

--- amr-web-app/src/containers/app/const.js
+++ amr-web-app/src/containers/app/const.js
@@ -10,4 +10,15 @@ const zoomableComponent = {
   CAMERA: 'Camera',
 }
 
-export { mainTabs, zoomableComponent }
+// Contains socket message type strings.
+const socketMessage = {
+  GENERAL_HEALTH: 'general_state',
+  SENSORS: 'sensors',
+  MOTORS: 'motors',
+
+  ODOMETRY: 'odometry',
+
+  TASKS: 'tasks',
+}
+
+export { mainTabs, zoomableComponent, socketMessage }
