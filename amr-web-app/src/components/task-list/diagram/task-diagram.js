0f790d3d7533fff1d56c23c3f15542c1f6f38460
Sebastian <sebastianliando@gmail.com>
2021-06-14 15:00:25 +0800

    Add animation when image is not available

--- amr-web-app/src/components/task-list/diagram/task-diagram.js
+++ amr-web-app/src/components/task-list/diagram/task-diagram.js
@@ -1,10 +1,28 @@
-import { Container, makeStyles } from '@material-ui/core'
-import React, { useMemo } from 'react'
+import { Container, makeStyles, Typography } from '@material-ui/core'
+import React, { useMemo, Fragment } from 'react'
+
+import Lottie from 'react-lottie'
+import noImageAnimation from '../../../assets/lottie/9354-image-viewer-icon-animation.json'
+
+console.log(noImageAnimation)
+
+const lottieOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: noImageAnimation,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice',
+  },
+}
 
 const taskDiagram = ({ base64Png, maxWidth = '100%', onClick }) => {
   const useStyles = useMemo(
     () =>
       makeStyles({
+        root: {
+          width: '100%',
+          textAlign: 'center',
+        },
         image: {
           maxWidth: maxWidth,
           cursor: onClick ? 'pointer' : 'auto',
@@ -16,14 +34,20 @@ const taskDiagram = ({ base64Png, maxWidth = '100%', onClick }) => {
   const classes = useStyles()
 
   return (
-    <Container onClick={onClick}>
+    <Container className={classes.root}>
       {base64Png ? (
         <img
+          onClick={onClick}
           src={`data:image/png;base64,${base64Png}`}
           className={classes.image}
           alt="Task Diagram"
         />
-      ) : null}
+      ) : (
+        <Fragment>
+          <Lottie options={lottieOptions} width={100} />
+          <Typography variant="h6">Waiting for Image...</Typography>
+        </Fragment>
+      )}
     </Container>
   )
 }
